52f75f5b9f6b6a152405b1e81663da61d373d986
Anay Gupta <777.anaycse@gmail.com>
2026-07-30 00:09:14 +0530

    feat(assets) : added handlers to send message and disconnect the client

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,12 @@ const wss = new WebSocketServer({port : 8080})
 
 const rooms = new Map<string , Set<CustomWebSocket>>()
 
+/* 
+"room124" : Set(1) {
+    Websocket {username : <> , currRoom : <>}
+}
+*/
+
 wss.on("connection" , (socket) => {
     
     socket.on("message" , (message : string) => {
@@ -15,7 +21,7 @@ wss.on("connection" , (socket) => {
         try {
             const data : IncomingMessage = JSON.parse(message.toString())
 
-            handleMessage(data)
+            handleMessage(socket , data)
         } catch (error) {
             console.error("Invalid JSON received")
         }
@@ -31,10 +37,79 @@ wss.on("connection" , (socket) => {
 
 } )
 
-function handleMessage(){
-    
+function handleMessage(ws : CustomWebSocket , data : IncomingMessage){
+    switch (data.type) {
+        case 'JOIN_ROOM': {
+            const {roomId , username} = data.payload
+
+            // attach info to socket
+
+            ws.roomId = roomId
+            ws.username = username
+
+            // if room dont exist
+            if(!rooms.has(roomId)){
+                rooms.set(roomId , new Set<CustomWebSocket>())
+            }
+
+            rooms.get(roomId)!.add(ws) // add the client to the designated room id
+
+            // !. means we are telling ts trust me this expression will not be null ior undefined stop telling me about type safety
+
+            broadcastToRoom(roomId , {
+                type : 'SYSTEM_MESSAGE' , 
+                payload : {text : `${username} joined the room`}
+            })
+            break;
+        }
+        case 'MESSAGE' :  {
+            if(!ws.username || !ws.roomId) return
+            
+            broadcastToRoom(ws.roomId , {
+                type : "NEW_MESSAGE" , 
+                payload : {
+                    username : ws.username , 
+                    text : data.payload.message , 
+                    timestamp : new Date().toISOString()
+                }
+            })
+            break
+        }
+    }
+
+
 }
 
-function handleDisconnect(){
+function broadcastToRoom(roomId : string , messageObj : OutgoingMessage){
+
+    const roomSockets = rooms.get(roomId)
+    if(!roomSockets)return
+    const msg = JSON.stringify(messageObj)
+
+    for (const element of roomSockets) {
+        if(element.readyState === WebSocket.OPEN){
+            element.send(msg)
+        }
+    }    
+}
+
+function handleDisconnect(ws : CustomWebSocket) : void{
+    const roomId = ws.roomId
+    if(!roomId || !rooms.has(roomId))return
+    
+    const roomSockets = rooms.get(roomId)!
+
+    roomSockets.delete(ws)
+
+    broadcastToRoom(roomId , {
+        type : 'SYSTEM_MESSAGE' , 
+        payload : {
+            text : `${ws.username} disconnected`
+        }
+    })
+
+    if(roomSockets.size === 0){
+        rooms.delete(roomId)
+    }
 
 }
