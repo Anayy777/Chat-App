b57b3649c3f27b740d85188fce6822d8364203b3
Anay Gupta <777.anaycse@gmail.com>
2026-08-02 22:03:27 +0530

    feat : changed the structure for user to choose between create and join

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,27 +1,25 @@
-export type OutgoingClientMessage = 
- {
-    type : "JOIN_ROOM" , 
-    payload : {
-        username : string
-        roomId : string
-
-    } 
-    
- } | 
- {
-        type : "CHAT_MESSAGE", 
-        payload : {
-            message : string
-        }
-}
-
-export type IncomingServerMessage =  {
-    type : "SYSTEM_MESSAGE" | "NEW_MESSAGE" , 
-    payload : {
-        username ?: string , 
-        text : string , 
-        timeStamp ?: string
+export type OutgoingClientMessage =
+  | {
+      type: 'JOIN_ROOM';
+      payload: {
+        username: string;
+        roomId: string;
+      };
     }
-}
+  | {
+      type: 'MESSAGE';
+      payload: {
+        message: string;
+      };
+    };
 
-export type connectionStatus = "CONNECTED" | "CONNECTING" | "DISCONNECTED"
+export type IncomingServerMessage = {
+  type: 'SYSTEM_MESSAGE' | 'NEW_MESSAGE';
+  payload: {
+    username?: string;
+    text: string;
+    timestamp?: string;
+  };
+};
+
+export type connectionStatus = 'CONNECTED' | 'CONNECTING' | 'DISCONNECTED';
